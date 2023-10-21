4a9e5d30daa98e2033ac3207a20d2d9c2d972334
nate <nathan+dev@electriccoin.co>
2023-10-20 17:59:08 -0700

    Type-checking stub of `impl RpcProvider for ZcashdClient`.

--- src/zcashdclient.rs
+++ src/zcashdclient.rs
@@ -1,2 +1,29 @@
-/// FIXME
+use crate::messages::get_info;
+use crate::RpcProvider;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tower::Service;
+
+/// A `zcashd` client which implements [RpcProvider]
 pub struct ZcashdClient {}
+
+/// An error during method execution
+pub struct ZcashdClientError {}
+
+impl RpcProvider for ZcashdClient {}
+
+impl Service<get_info::Request> for ZcashdClient {
+    type Response = get_info::Response;
+    type Error = ZcashdClientError;
+    // FIXME: Replace with a concrete type?
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        todo!()
+    }
+
+    fn call(&mut self, _req: get_info::Request) -> Self::Future {
+        todo!()
+    }
+}
