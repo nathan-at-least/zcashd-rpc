81fba86d9f91988e9291000f88e6ac0df82763b4
nate <nathan+dev@electriccoin.co>
2023-10-26 11:25:55 -0700

    Start factoring out `zcashd_regtest_controller::setup::setup` into functional stages.

--- regtest-controller/src/setup.rs
+++ regtest-controller/src/setup.rs
@@ -1,31 +1,16 @@
 use crate::ZcashdConfig;
 use anyhow::Context;
 use indoc::indoc;
+use std::path::Path;
 use tokio::process::Child;
 
 pub(crate) async fn setup() -> anyhow::Result<(Child, ZcashdConfig)> {
-    use tokio::process::Command;
-
     let datadir =
         tempfile::TempDir::with_prefix(format!("{}_regtest_node.", env!("CARGO_PKG_NAME")))?
             .into_path();
 
-    tokio::fs::write(
-        datadir.join("zcash.conf"),
-        indoc! { r#"
-            regtest=1
-            server=1
-        "# },
-    )
-    .await?;
-
-    let mut cmd = Command::new("zcashd");
-    cmd.arg(format!("-datadir={}", datadir.display()));
-    cmd.current_dir(&datadir);
-
-    let command_description = format!("command: {:?}", cmd.as_std());
-
-    let child = cmd.spawn().context(command_description.clone())?;
+    write_config(&datadir).await?;
+    let (child, command_description) = launch_child(&datadir)?;
 
     let config = ZcashdConfig {
         datadir,
@@ -33,4 +18,29 @@ pub(crate) async fn setup() -> anyhow::Result<(Child, ZcashdConfig)> {
     };
 
     Ok((child, config))
+}
+
+async fn write_config(datadir: &Path) -> std::io::Result<()> {
+    tokio::fs::write(
+        datadir.join("zcash.conf"),
+        indoc! { r#"
+            regtest=1
+            server=1
+        "# },
+    )
+    .await
+}
+
+fn launch_child(datadir: &Path) -> anyhow::Result<(Child, String)> {
+    use tokio::process::Command;
+
+    let mut cmd = Command::new("zcashd");
+    cmd.arg(format!("-datadir={}", datadir.display()));
+    cmd.current_dir(datadir);
+
+    let command_description = format!("command: {:?}", cmd.as_std());
+
+    let child = cmd.spawn().context(command_description.clone())?;
+
+    Ok((child, command_description))
 }
